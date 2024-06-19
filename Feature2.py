2daf032f5bc29264b4a612e5803c78512d6d687a
Vikas Negi <vikas.negi@Nykaa.local>
2024-06-19 20:33:06 +0530

    define spark_session

--- Feature2.py
+++ Feature2.py
@@ -1,2 +1,12 @@
 # Feature2
 from pyspark.sql import SparkSession
+
+def spark_session():
+    return SparkSession. \
+        builder. \
+        config('spark.ui.port', '0'). \
+        config('spark.shuffle.useOldFetchProtocol', 'true'). \
+        enableHiveSupport(). \
+        master('yarn'). \
+        getOrCreate()
+        
